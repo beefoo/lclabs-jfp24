ce760628f42c08986553e6f1a752bb1b910ac14e
shaunakh24 <shaunakayharrison@gmail.com>
2024-07-02 12:16:13 -0400

    Update and clean step 1 #31

--- workflow/workflow_helpers.py
+++ workflow/workflow_helpers.py
@@ -1,13 +1,27 @@
 import csv
-import re
+import requests
+import os, os.path
 
 
 def read_in_collection_csv_for_links(file, encoding='utf-8'):
-    """Function explanation."""
+    """Load in collection csv (file)"""
     item_links = []
-
     with open(file, 'r', encoding=encoding) as links:
         item = csv.reader(links)
         for item_row_data in item:
             item_links.append(item_row_data)
     return item_links
+
+def access_and_store_json(link):
+    """Check for <200> response and raise exception error if you can't connect to API."""
+    get_image = requests.get(link + '?fo=json', timeout=15)
+    if get_image.status_code == 200:
+        response = get_image.json()
+    return response
+
+def request_link(url):
+    """Return the JSON of a fully formatted link."""
+    get_image = requests.get(url, timeout=15)
+    if get_image.status_code == 200:
+        response = get_image.json()
+    return response
