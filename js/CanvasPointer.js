34aa991de51f2e2ca1dc7f229580fb5a1a3c710f
beefoo <brian@youaremyjoy.org>
2024-07-07 16:12:28 -0400

    implement segment rotation

--- js/CanvasPointer.js
+++ js/CanvasPointer.js
@@ -5,6 +5,7 @@ export default class CanvasPointer {
         const defaults = {
             debug: false,
             elements: [],
+            mode: 'move',
             startX: 0,
             startY: 0,
         };
@@ -14,22 +15,51 @@ export default class CanvasPointer {
 
     init() {
         const {
-            canvas, elements, startX, startY,
+            canvas, elements, mode, startX, startY,
         } = this.options;
-        this.start = { x: startX, y: startY };
-        this.elements = elements;
         this.canvas = canvas;
+        this.elements = elements;
+        this.mode = mode;
+        this.start = { x: startX, y: startY };
         this.canvasRect = canvas.getBoundingClientRect();
+        this.onPointerDown();
+    }
+
+    onManipulate(xy) {
+        const [segment] = this.elements;
+        const { el, rotationStart } = segment;
+        const rotation = Helper.degreesBetweenPoints(xy, this.segmentCenter);
+        const rotationDelta = rotation - this.rotationStart;
+        const newRotation = (rotationStart + rotationDelta) % 360.0;
+        el.style.transform = `rotate3d(0, 0, 1, ${newRotation}deg)`;
+        this.newRotation = newRotation;
+    }
+
+    onManipulateEnd(xy) {
+        const [segment] = this.elements;
+        const { el } = segment;
+        el.setAttribute('data-rotation', this.newRotation);
+    }
+
+    onManipulateStart() {
+        const [segment] = this.elements;
+        const s = segment.el.getBoundingClientRect();
+        this.segmentCenter = { x: s.x + s.width * 0.5, y: s.y + s.height * 0.5 };
+        this.rotationStart = Helper.degreesBetweenPoints(this.start, this.segmentCenter);
     }
 
     onMove(xy) {
         const c = this.canvasRect;
         const { start } = this;
         const delta = { x: xy.x - start.x, y: xy.y - start.y };
-        this.elements.forEach((element) => {
+        this.elements.forEach((element, i) => {
             const { el, startX, startY } = element;
-            el.style.left = `${Helper.pxToPercent(startX + delta.x, c.width)}%`;
-            el.style.top = `${Helper.pxToPercent(startY + delta.y, c.height)}%`;
+            const left = Helper.pxToPercent(startX + delta.x, c.width);
+            const top = Helper.pxToPercent(startY + delta.y, c.height);
+            el.style.left = `${left}%`;
+            el.style.top = `${top}%`;
+            this.elements[i].x = left;
+            this.elements[i].y = top;
         });
     }
 
@@ -56,13 +86,39 @@ export default class CanvasPointer {
                 const my2 = c.height - c.height * padding;
                 const newX = Math.min(Math.max(mx1, x1), mx2);
                 const newY = Math.min(Math.max(my1, y1), my2);
+                const left = Helper.pxToPercent(newX, c.width);
+                const top = Helper.pxToPercent(newY, c.height);
                 el.classList.add('animating');
-                el.style.left = `${Helper.pxToPercent(newX, c.width)}%`;
-                el.style.top = `${Helper.pxToPercent(newY, c.height)}%`;
+                el.style.left = `${left}%`;
+                el.style.top = `${top}%`;
+                el.setAttribute('data-x', left);
+                el.setAttribute('data-y', top);
                 setTimeout(() => {
                     el.classList.remove('animating');
                 }, 550);
+            } else {
+                el.setAttribute('data-x', element.x);
+                el.setAttribute('data-y', element.y);
             }
         });
+    }
+
+    onMoveStart() {
+
+    }
+
+    onPointerDown() {
+        if (this.mode === 'manipulate') return this.onManipulateStart();
+        return this.onMoveStart();
+    }
+
+    onPointerMove(xy) {
+        if (this.mode === 'manipulate') return this.onManipulate(xy);
+        return this.onMove(xy);
+    }
+
+    onPointerUp(xy) {
+        if (this.mode === 'manipulate') return this.onManipulateEnd(xy);
+        return this.onMoveEnd(xy);
     }
 }
--- js/Helper.js
+++ js/Helper.js
@@ -1,4 +1,11 @@
 export default class Helper {
+    static degreesBetweenPoints(p1, p2) {
+        const dy = p2.y - p1.y;
+        const dx = p2.x - p1.x;
+        const radians = Math.atan2(dy, dx);
+        return (radians * 180.0) / Math.PI;
+    }
+
     static pxToPercent(px, containerPx) {
         return (px / containerPx) * 100;
     }
