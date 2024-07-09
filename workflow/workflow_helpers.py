bdfcfcbf85ebed933bdc6adce6a27fd5c35a1435
aisaiahpellecer <pellecer.aisaiah8@gmail.com>
2024-07-09 11:28:50 -0500

    step 1 consolidated #31

--- workflow/workflow_helpers.py
+++ workflow/workflow_helpers.py
@@ -14,20 +14,14 @@ def read_in_collection_csv_for_links(file, encoding='utf-8'):
 
 def access_and_store_json(link):
     """Check for <200> response and raise exception error if you can't connect to API."""
-    get_image = requests.get(link + '?fo=json', timeout=30)
+    get_image = requests.get(link + '?fo=json', timeout=15)
     if get_image.status_code == 200:
         response = get_image.json()
     return response
 
 def request_link(url):
-    try:
-        """Return the JSON of a fully formatted link."""
-        get_image = requests.get(url,timeout=30)
-        if get_image.status_code == 200:
-            response = get_image.json()
-        return response
-    except Exception as e:
-        print(f"An error occurred while requesting URL: {url}. Error: {e}")
-        return None
-
-        
+    """Return the JSON of a fully formatted link."""
+    get_image = requests.get(url, timeout=15)
+    if get_image.status_code == 200:
+        response = get_image.json()
+    return response
