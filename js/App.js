acd9b7c54af2e6680ebaae7ca3af1803930389bb
beefoo <bfoo@loc.gov>
2024-08-06 09:27:06 -0400

    Canvas tools boilerplate

--- js/App.js
+++ js/App.js
@@ -1,4 +1,5 @@
 import Canvas from './Canvas.js';
+import CanvasTools from './CanvasTools.js';
 import PhotoBrowser from './PhotoBrowser.js';
 
 export default class App {
@@ -12,6 +13,7 @@ export default class App {
 
     init() {
         const canvas = new Canvas(this.options);
+        const canvas_tools = new CanvasTools(this.options);
         const photo_browser = new PhotoBrowser(this.options);
         this.initialized = true;
     }
